a15c57a51093e44b04e1e2b115899ace95e18f23
Vamma <58887108+Vam-ma@users.noreply.github.com>
2022-11-13 13:16:39 +0200

    Add files via upload

---
+++ WarMod/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Vamma'
+description 'Capture the flag style warmod'
+version '1.0.0'
+
+client_script 'warMod.net.dll'
+server_script 'warMod_Server.net.dll'
