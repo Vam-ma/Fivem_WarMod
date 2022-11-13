848e22cc8ead47c28789601c83fc73cd5c258ce6
Vamma <58887108+Vam-ma@users.noreply.github.com>
2022-11-13 18:24:42 +0200

    Add files via upload

--- WarMod/fxmanifest.lua
+++ WarMod/fxmanifest.lua
@@ -5,5 +5,13 @@ author 'Vamma'
 description 'Capture the flag style warmod'
 version '1.0.0'
 
+ui_page "ui/index.html"
+
+files {
+    "ui/index.html",
+    "ui/style.css",
+    "ui/ui.js"
+}
+
 client_script 'warMod.net.dll'
 server_script 'warMod_Server.net.dll'
