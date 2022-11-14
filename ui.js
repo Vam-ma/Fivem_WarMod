a7a04554a1ba28b5ccef5d0f5d996624990e8867
Vamma <58887108+Vam-ma@users.noreply.github.com>
2022-11-14 17:03:05 +0200

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,59 @@
+var header = document.getElementById("header")
+var section_left = document.getElementById("left")
+var section_center = document.getElementById("center")
+var section_right = document.getElementById("right")
+var team1 = document.getElementById("team1")
+var team2 = document.getElementById("team2")
+var teamheader = document.getElementById("teamname")
+
+var teams = ["Team1","Team2"]
+var state = true
+
+const input = document.querySelector('input');
+
+AddEventListeners(team1,"rgba(218,232,252,1)","rgba(190,202,219,1)",0)
+AddEventListeners(team2,"rgba(248,206,204,1)","rgba(191,159,157,1)",1)
+Hideobj(section_left);
+Hideobj(section_right);
+window.addEventListener("message",function(event){
+    if(state){
+        $('#article').show();
+    } else{
+        $('#article').hide();
+    }
+})
+
+function AddEventListeners(obj, basecolor, highlightColor,team){
+    obj.addEventListener("mouseover", function()
+    {
+        obj.style.backgroundColor=highlightColor;
+    });
+    obj.addEventListener("mouseleave", function()
+    {
+        obj.style.backgroundColor=basecolor; 
+    });
+    obj.addEventListener("click", function()
+    {
+        Hideobj(team1);
+        Hideobj(team2);
+        Showobj(section_left);
+        Showobj(section_right);
+        PickTeam(team)
+    });
+}
+input.addEventListener('keypress', getKey);
+
+function getKey(key){
+    if(key.code === 27){
+        $.post('http://warmod/keyevent', JSON.stringify("27"))
+    }
+}
+function Hideobj(obj){
+    obj.style.display = "none"
+}
+function Showobj(obj){
+    obj.style.display = "block"
+}
+function PickTeam(team){
+teamheader.innerHTML = teams[team];
+}
